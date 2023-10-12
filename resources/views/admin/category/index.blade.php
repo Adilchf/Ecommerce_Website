584e90bf1f54f96e11bbad960920fd7dcf21cff4
adelchf <adelchellaf7@gmail.com>
2023-10-12 19:09:41 +0100

    update

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,9 +2,39 @@
 
 @section('content')
 <div class="card">
+    <div class="card-header">
+        <h4>Category page</h4>
+    </div>
     <div class="card-body">
-        <h1>Adel app</h1>
-
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($category as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->description }}</td>
+                    <td>
+                        <img src="{{ asset('assets/uploads/category/'.$item->image)}}" alt="Image here">
+                    </td>
+                    <td>
+                        <button class="btn btn-primary">Edit</button>
+                        <button class="btn btn-primary">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+            @endforeach
+        </table>
     </div>
 </div>
+
+
 @endsection
